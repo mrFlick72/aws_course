3581c667617aefcc7c1f90837af5fde236e2753a
mrFlick72 <valerio.vaudi@gmail.com>
2021-01-13 01:02:40 +0100

    terraform example of course role

---
+++ iam/terraform.tf
@@ -0,0 +1,17 @@
+provider "aws" {}
+
+resource "aws_iam_role" "aws_course_role" {
+  name = "aws_course_role"
+
+  assume_role_policy = file("trustPolicy.json")
+
+  tags = {
+    Name = "aws_course_role"
+    scope = "aws_course"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "aws_course_role_attach" {
+  role       = aws_iam_role.aws_course_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
