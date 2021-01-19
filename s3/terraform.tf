bf35cbf986c0beb51fdaada7eabc7ea85ce377ef
mrFlick72 <valerio.vaudi@gmail.com>
2021-01-19 21:39:48 +0100

    web hosting on S3

--- s3/terraform.tf
+++ s3/terraform.tf
@@ -1,9 +1,23 @@
 provider "aws" {}
 
-resource "aws_s3_bucket" "bucket_name" {
-  bucket = var.bucket_name
-  acl = "private"
+data "template_file" "policy" {
+  template = file("public_policy.json")
+  vars = {
+    bucket_name = var.bucket_name
+  }
+}
 
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  acl = "public-read"
+
+
+  policy = data.template_file.policy.rendered
+  website {
+
+    index_document = "index.html"
+    error_document = "error.html"
+  }
   tags = {
     Name = "aws_course_role"
     scope = "aws_course"
@@ -11,13 +25,19 @@ resource "aws_s3_bucket" "bucket_name" {
 }
 
 resource "aws_s3_bucket_object" "profile" {
-  bucket = aws_s3_bucket.bucket_name.bucket
+  bucket = aws_s3_bucket.bucket.bucket
   key = "static/images/profile.png"
   source = var.image_path
 }
 
 resource "aws_s3_bucket_object" "index" {
-  bucket = aws_s3_bucket.bucket_name.bucket
+  bucket = aws_s3_bucket.bucket.bucket
   key = "index.html"
-  source = var.default_object_path
+  source = var.default_website_index
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket = aws_s3_bucket.bucket.bucket
+  key = "error.html"
+  source = var.default_website_error
 }
--- s3/variables.tf
+++ s3/variables.tf
@@ -1,3 +1,4 @@
 variable "bucket_name" {}
 variable "image_path" {}
-variable "default_object_path" {}
+variable "default_website_index" {}
+variable "default_website_error" {}
