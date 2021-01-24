60b2566cc23d8d7c3e4a776d6dd6cda7d4aecbb3
mrFlick72 <valerio.vaudi@gmail.com>
2021-01-24 13:30:46 +0100

    WIP EC2 packer file iam instance profile

--- iam/terraform.tf
+++ iam/terraform.tf
@@ -11,7 +11,17 @@ resource "aws_iam_role" "aws_course_role" {
   }
 }
 
+resource "aws_iam_instance_profile" "aws_course_role_instance_profile" {
+  name = "aws_course_role"
+  role = aws_iam_role.aws_course_role.name
+}
+
 resource "aws_iam_role_policy_attachment" "aws_course_EC2_role_attach" {
   role       = aws_iam_role.aws_course_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "aws_course_S3_role_attach" {
+  role       = aws_iam_role.aws_course_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
