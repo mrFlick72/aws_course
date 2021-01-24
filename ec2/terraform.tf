f9c869e9b5f5ff5fcbaf19b8b318a29ecb38bde7
mrFlick72 <valerio.vaudi@gmail.com>
2021-01-24 16:33:17 +0100

    EC2 terraform provisioner

---
+++ ec2/terraform.tf
@@ -0,0 +1,32 @@
+provider "aws" {}
+
+resource "aws_security_group" "autoscaling_hello_world_sg" {
+  name = "autoscaling_hello_world_sg"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 8080
+    protocol = "TCP"
+    to_port = 8080
+  }
+}
+
+resource "aws_instance" "aws_course_ec2_instance" {
+  depends_on = [
+    aws_security_group.autoscaling_hello_world_sg]
+  ami = var.ami
+  instance_type = var.instance_type
+  iam_instance_profile = "aws_course_role"
+  associate_public_ip_address = true
+  key_name = var.key_name
+  subnet_id = var.subnet_id
+
+  vpc_security_group_ids = [
+    aws_security_group.autoscaling_hello_world_sg.id
+  ]
+
+  tags = {
+    "Name":"Autoscaling Hello World"
+  }
+
+}
